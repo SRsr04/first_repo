ebb4f21cad02f0ac2c73a6ba0096b71805e3fd34
Synyshyn Roman <synyshynroman@gmail.com>
2023-09-20 10:26:03 +0300

    Update hw6

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import sys
 from pathlib import Path
+import shutil
+import zipfile
 
-CATEGORIES = {"Audio": [".mp3"],
-              "Video": [".mp4"],
-              "Docs": [".txt", ".docx", ".pdf"]
-              }
+CATEGORIES = {
+    "Audio": [".mp3"],
+    "Video": [".mp4"],
+    "Docs": [".txt", ".docx", ".pdf"],
+    "Images": [".jpg", ".png", ".gif"],
+    "Archives": [".zip", ".rar"],
+}
 
-def get_categories(file:Path) -> str:
+def get_categories(file: Path) -> str:
     ext = file.suffix.lower()
     for cat, exts in CATEGORIES.items():
         if ext in exts:
@@ -15,21 +20,32 @@ def get_categories(file:Path) -> str:
 
 def move_file(file: Path, category: str, root_dir: Path) -> None:
     target_dir = root_dir.joinpath(category)
-    print (target_dir.exists())
     if not target_dir.exists():
-        target_dir.mkdir()
-    file.rename(target_dir.joinpath(file.name))
-    
+        target_dir.mkdir(parents=True)
+    new_file_path = target_dir.joinpath(file.name)
+    if not new_file_path.exists():
+        file.rename(new_file_path)
+    else:
+        # Handle duplicate file names by adding a unique identifier
+        count = 1
+        while True:
+            new_name = f"{file.stem}_{count}{file.suffix}"
+            new_file_path = target_dir.joinpath(new_name)
+            if not new_file_path.exists():
+                file.rename(new_file_path)
+                break
+            count += 1
 
+def extract_archive(file: Path, extraction_dir: Path):
+    with zipfile.ZipFile(file, 'r') as zip_ref:
+        zip_ref.extractall(extraction_dir)
 
-def sort_folder(path:Path) -> None:
+def sort_folder(path: Path) -> None:
     for item in path.glob("**/*"):
         if item.is_file():
             category = get_categories(item)
             move_file(item, category, path)
-
-    for item in path.glob("**/*"):
-        if item.is_file() and item.stat().st_size == 0:
+        elif item.is_file() and item.stat().st_size == 0:
             item.unlink()
 
 def main() -> str:
@@ -39,13 +55,11 @@ def main() -> str:
         return "No path to folder"
         
     if not path.exists():
-        return "Folder does not exists"
-
+        return "Folder does not exist"
 
     sort_folder(path)
 
-    return "All right"
+    return "All done"
 
-    
 if __name__ == '__main__':
     print(main())
